d127750973e128f88978c2b960c7e9b9e05b4d65
WillyAgustri <willyofficial082@gmail.com>
2023-10-19 00:36:29 +0700

    use diffcallback

--- app/src/main/java/com/example/githubapplication/UserAdapter.kt
+++ app/src/main/java/com/example/githubapplication/UserAdapter.kt
@@ -38,12 +38,11 @@ class UserAdapter : RecyclerView.Adapter<UserAdapter.ListViewHolder>() {
     }
 
     fun submitList(user: List<ItemsItem>) {
-        val diffCallback = UserDiffCallback(listUser, user)
+        val diffCallback = DiffUtilCallback(listUser, user)
         val diffResult = DiffUtil.calculateDiff(diffCallback)
-
         listUser.clear()
         listUser.addAll(user)
-        notifyDataSetChanged()
+        diffResult.dispatchUpdatesTo(this)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
@@ -64,5 +63,36 @@ class UserAdapter : RecyclerView.Adapter<UserAdapter.ListViewHolder>() {
 
     fun interface OnItemClickCallback {
         fun onItemClicked(data: ItemsItem)
+    }
+
+    class DiffUtilCallback(private val oldList: List<ItemsItem>, private val newList: List<ItemsItem>) :
+        DiffUtil.Callback() {
+
+        override fun getOldListSize(): Int {
+            return oldList.size
+        }
+
+        override fun getNewListSize(): Int {
+            return newList.size
+        }
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldList[oldItemPosition]
+            val newItem = newList[newItemPosition]
+
+            return oldItem.javaClass == newItem.javaClass
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldList[oldItemPosition]
+            val newItem = newList[newItemPosition]
+
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+        @Override
+        override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+            return super.getChangePayload(oldItemPosition, newItemPosition)
+        }
     }
 }
